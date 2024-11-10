ea1278142c49d7c1cf8c4cf7d08c7bdda5014e31
zain ahmed <zain.ahmed199524@gmail.com>
2024-11-10 14:46:50 +0800

    new branch

--- src/App.js
+++ src/App.js
@@ -50,7 +50,9 @@ useEffect(()=>{
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-     
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
